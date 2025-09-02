8b22a88de9604e8d2ec5bcfdd33de3d2098c57d6
rafa-as <ppax325@gmail.com>
2025-09-02 23:01:51 +0700

    audio

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const audio = document.getElementById("myAudio");
 const volumeSlider = document.getElementById("volume");
+const btn = document.getElementById("controlButton");
+
 
 // set awal
 audio.volume = 0.5;
@@ -7,4 +9,12 @@ audio.volume = 0.5;
 // ubah volume pas slider digeser
 volumeSlider.addEventListener("input", () => {
 	audio.volume = volumeSlider.value;
+});
+
+btn.addEventListener("click", () => {
+if (audio.paused) {
+	audio.play();
+} else {
+	audio.pause();
+}
 });
